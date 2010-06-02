79eee4b76c84c1b5126445b2c196748989bf67b7
Sebastian Billaudelle <stein@cream-project.org>
2010-06-02 21:27:54 +0200

    Minor stuff.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name = 'Melange Contrib Libraries',
     version = '0.1',
     package_dir = {'cream.contrib.melange': 'melange'},
     packages = ['cream.contrib.melange'],
-    include_package_data = True,
     package_data={'cream.contrib.melange': ['interface.glade']}
     )
